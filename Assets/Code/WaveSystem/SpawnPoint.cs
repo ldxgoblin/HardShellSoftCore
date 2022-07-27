41e676b378424aaf1643c2c33d63ddb1f312e872
shc3x6 <superhardcode666@gmail.com>
2022-07-28 00:07:55 +0200

    Wave Manager Upgrades, small fixes

--- Assets/Code/WaveSystem/SpawnPoint.cs
+++ Assets/Code/WaveSystem/SpawnPoint.cs
@@ -7,8 +7,30 @@ public class SpawnPoint : MonoBehaviour
     public bool canSpawn;
     public float spawnRate;
     
-    public GameObject Spawn(GameObject enemy, Transform enemyTransform)
+    public GameObject Spawn(GameObject enemy, Transform parent)
     {
-        return Instantiate(enemy, transform.position, Quaternion.identity, enemyTransform);
+        return Instantiate(enemy, transform.position, Quaternion.identity, parent);
+    }
+
+    public void SpawnMultipleWithDelay(Wave wave, Transform parent)
+    {
+        StartCoroutine(Spawn(wave, parent));
+    }
+
+    private IEnumerator Spawn(Wave wave, Transform parent)
+    {
+        GameObject enemyType = wave.enemyType;
+        
+        for (int i = 0; i < wave.enemyCountPerSpawnPoint; i++)
+        {
+            var enemyGo = Instantiate(enemyType, transform.position, Quaternion.identity, parent);
+            var enemy = enemyGo.gameObject.GetComponent<Enemy>();
+            
+            // bump enemy away from spawnpoint to prevent overlapping
+            enemy.Bump();
+
+            yield return new WaitForSeconds(spawnRate);
+        }
+
     }
 }
