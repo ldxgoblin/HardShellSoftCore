a63baa5787902569f122e3868af6a3436d67eefd
shc3x6 <superhardcode666@gmail.com>
2022-09-08 00:27:10 +0200

    Main Menu (basic), Post Processing

--- Assets/Code/UI/UIButtons.cs
+++ Assets/Code/UI/UIButtons.cs
@@ -7,11 +7,21 @@ public class UIButtons : MonoBehaviour
     public static event Action OnQuitButton;
     public void RestartGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
     {
         OnQuitButton?.Invoke();
+    }
+    
+    public void QuitGameImmediately()
+    {
+        Application.Quit();
     }
 }
